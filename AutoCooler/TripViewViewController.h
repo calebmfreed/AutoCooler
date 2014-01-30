aa113464339747da79444f6b6a10976bed241b80
calebmfreed <calebmfreed@gmail.com>
2014-01-29 19:14:29 -0600

    Lots of updates

---
+++ AutoCooler/TripViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  TripViewViewController.h
+//  AutoCooler
+//
+//  Created by Caleb Freed on 1/29/14.
+//  Copyright (c) 2014 Caleb Freed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TripViewViewController : UITableViewController
+
+@end
